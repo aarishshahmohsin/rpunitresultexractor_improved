f591c2370ce8a9da9b6dc3d1438abacbb54ef97d
Aarish Shah Mohsin <aarishshah1@gmail.com>
2023-07-22 22:24:46 +0530

    fixed output.csv bug

--- runner.py
+++ runner.py
@@ -1,8 +1,13 @@
 import csv
 from scraper import fetch_cpi
+import os
 
 csv_file = "students.csv"
 output_file = "output.csv"
+
+if (not os.path.exists):
+    os.system("touch {}".format(output_file))
+
 with open('output.csv', 'w', newline='') as output_file:
     writer = csv.writer(output_file)
     with open(csv_file) as file_obj:
